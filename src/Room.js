7e01c96bfeb0ffc0e4b54df43fbfa17ff7b8aaf9
Taimour <taimour.nazar1@gmail.com>
2020-07-09 12:52:57 +0500

    Assignment_Completed

--- src/Room.js
+++ src/Room.js
@@ -6,7 +6,7 @@ function Room() {
     //console.log("State = ",state);
     let [isLit, setLit]=useState(false);
     let [age, setAge] = useState(24);
-
+    let [temperature, setTemperature] = useState(72);
     //let hello ='Hello '+ isLit +'world';
     //let hello = 'Hello ${isLit} world';
 
@@ -14,9 +14,14 @@ function Room() {
         <div className={`room ${isLit?"lit":"dark"}`}>
         This Room is {isLit?"lit":"dark"}
         <br/>
+        <button onClick={()=> setLit(true) }>ON</button>&nbsp;
+        <button onClick={()=> setLit(false) }>OFF</button>
+        <br/><br/>
+        Temperature: {temperature}&nbsp;
+        <button onClick={()=>setTemperature(++temperature)}>+</button>&nbsp;
+        <button onClick={()=>setTemperature(--temperature)}>-</button>
+        <br/><br/>
         Age : {age}
-        <br/>
-        <button onClick={()=> setLit(!isLit) }>Toogle Light</button>
         <br/>
         <button onClick={() => {
                     console.log("Increase age arrow function");
